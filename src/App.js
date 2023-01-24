7755e6d24ae0cf310573360ecb259efaf6a76fd8
dongmin7208 <sayg3o3d@gmail.com>
2023-01-24 15:00:16 +0900

    ADD: github pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/hello'>
+        <Route path='/about-jp'>
           <h1>Hello</h1>
         </Route>
         <Route path='/movie/:id'>
